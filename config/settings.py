eaa592c63012d4ec8e7671b94d98360b3cf131fa
AmirReza Nasiri <AmirRezaNasiri830@gmail.com>
2021-04-29 14:10:55 +0430

    for fandogh

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,9 @@ LOGIN_URL = "login"
 SECRET_KEY = '3!3yk^-m6gfn+i-8m97!p=u)25w!%i#=ng3h#5fxmmp$w5j%u='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tukan-nasiri.fandogh.cloud',]
 
 
 # Application definition
@@ -95,11 +95,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '######',
+        'USER': '######',
+        'PASSWORD': '######',
+        'HOST': '######',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -138,15 +140,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-AUTH_USER_MODEL = 'account.User'
 
+AUTH_USER_MODEL = 'account.User'
 #---
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #______________________-------email config-----___________________________
